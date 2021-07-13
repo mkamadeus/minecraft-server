76c11149483eb21e7e091c06fa032fd184405275
mkamadeus <13518035@std.stei.itb.ac.id>
2021-07-13 22:49:26 +0700

    fix: fixed output for server and ssh key

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
-output "ipv4_address" {
-  value       = vultr_instance_ipv4.minecraft_ipv4.ip
+output "server_address" {
+  value       = vultr_instance.minecraft.main_ip
   description = "The server's IP address"
+}
+
+output "private_ssh_key" {
+  value       = tls_private_key.generated.private_key_pem
+  description = "Private key for the server"
+  sensitive   = true
 }
